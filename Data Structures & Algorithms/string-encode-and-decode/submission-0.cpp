c77ce396b85a47758ec37e4a4f972707b42a9862
DamianKuras <damian_kuras@outlook.com>
2026-05-03 00:00:53 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded;
+        for (const string& s : strs) {
+            encoded += to_string(s.size()) + "#" + s;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded;
+        int i = 0;
+
+        while (i < s.size()) {
+            int j = i;
+            while (s[j] != '#') j++;
+
+            int len = stoi(s.substr(i, j - i));
+            i = j + 1;
+            decoded.push_back(s.substr(i, len));
+            i += len;
+        }
+
+        return decoded;
+    }
+};
